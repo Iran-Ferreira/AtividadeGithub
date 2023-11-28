29942e5c5e0266ed88d3029dc3617011e0e53731
ClarisseKarolina <112405839+ClarisseKarolina@users.noreply.github.com>
2023-11-28 10:28:26 -0300

    função calcular - Clarissse Karolina

--- atividade_1/src/code/scripts.js
+++ atividade_1/src/code/scripts.js
@@ -14,3 +14,14 @@ function back(){
     const resultado = document.getElementById("resultado").innerHTML
     document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1)
 }
+
+
+fuction calcular(){
+    const resultado = document.getElementById("resultado").innerHTML
+    if(resultado){
+        document.getElementById("resultado").innerHTML = eval(resultado)
+    }else{
+        document.getElementById("resultado").innerHTML = "nada..."
+    }
+    
+}
