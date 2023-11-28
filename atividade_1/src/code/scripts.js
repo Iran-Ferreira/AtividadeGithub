86775858ab375fcbcb33cdc817b5754492301ef2
KamilySilva <kamilyinez@gmail.com>
2023-11-28 02:23:29 -0300

    funções insert e back

--- atividade_1/src/code/scripts.js
+++ atividade_1/src/code/scripts.js
@@ -1,3 +1,15 @@
 function clean(){
     document.getElementById("resultado").innerHTML = ''
 }
+
+// Função de inserir os números da operação
+function insert(num){
+    const numero = document.getElementById("resultado").innerHTML
+    document.getElementById("resultado").innerHTML = numero + num
+}
+
+// Função que exclui o último valor do campo
+function back(){
+    const resultado = document.getElementById("resultado").innerHTML
+    document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1)
+}
